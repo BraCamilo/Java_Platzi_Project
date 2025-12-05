5cf603f85c9ef27916e92e5dc272ef71cc8c98af
BraCamilo <brayancalvo64@outlook.com>
2025-12-05 15:21:41 -0500

    Se hicieron cambios para hacer uso de metodos get y set

--- src/Mi_Platzi_Project/Contenido/Pelicula.java
+++ src/Mi_Platzi_Project/Contenido/Pelicula.java
@@ -1,37 +1,60 @@
 package Mi_Platzi_Project.Contenido;
 
-public class Pelicula {
-    public String titulo;
-    public String descripcion;
-    public String duracion;
-    public String genero;
-    public String year;
-    public double calificacion;
-    public boolean disponible;
+import org.w3c.dom.ls.LSOutput;
 
+public class Pelicula {
+    //Atributos
+    private String titulo;
+    private String descripcion;
+    private String duracion;
+    private String genero;
+    private String year;
+    private double calificacion;
+    private boolean disponible;
+
+    //Getters
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getYear(){
+        return year;
+    }
+    public double getCalificacion(){
+        return calificacion;
+    }
+    public boolean getDisponible(){
+        return disponible;
+    }
+
+    //Setters
+    public void setTitulo(String newTitulo){
+        this.titulo = newTitulo;
+    }
+    public void setYear(String newYear){
+        this.year = newYear;
+    }
+    public void setDisponible(){
+        this.disponible = disponible;
+    }
 
     //Metodos
     public void reproducir(){
         System.out.println("Reproduciendo "+ titulo);
     }
-
     public String obtener(){
-        return titulo + "(" + year + ")\n" +
-                "Genero: " + genero + "\n" +
-                "Duracion: " + duracion + "\n" +
-                "Descripcion: " + descripcion + "\n" +
-                "Calificacion: " + calificacion + "5/" + "\n" +
-                "Disponible: " + disponible + ".";
+        return "El titulo de la pelicula es: "+ getTitulo()+"\nEl ano de estreno es: " + getYear() + "\nDispobinilidad: " + getDisponible() +"\nY su actual calificacion es: " + getCalificacion();
     }
-    //El color lila hace referencia a que se esta mencionando el atributo de la clase y el blanco como tal a la variable que se esta usando en la funcion
     public void calificar(double calificacion) {
         if (calificacion >= 0 && calificacion <= 5){
             this.calificacion = calificacion;
         }
 
     }
-
     public boolean esPopular(){
         return calificacion >= 4;
     }
 }
+
+
+//COMENTARIOS
+//El color lila hace referencia a que se esta mencionando el atributo de la clase y el blanco como tal a la variable que se esta usando en la funcion
